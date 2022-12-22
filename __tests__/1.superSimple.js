15269da8de5fdc7d5a38a5b064afcab375d707a5
Ashish Verma <113393491+the3rdeyecan@users.noreply.github.com>
2022-12-22 12:18:48 -0700

    created supersimple test app

--- __tests__/1.superSimple.js
+++ __tests__/1.superSimple.js
@@ -1,10 +1,22 @@
-describe('simple tests', ()=>{
-    describe('first tests', ()=>{
-        it('should pass', ()=>{
-            expect(true).toBeTruthy()
-        })
-        // it('should fail', ()=>{
-        //     expect(true).not.toBeTruthy()
-        // })
-    })
-})
+describe('Arrays', () => {
+  it('should increase lenght after push', () => {
+    //setup
+
+    let newArray = [];
+    let newItem = 'Hello';
+
+    //execute
+    newArray.push(newItem);
+    //validate
+    expect(newArray.length).toBe(1);
+  });
+
+  it("should have the type 'array' ", () => {
+    let newArray = [];
+
+    expect(Array.isArray(newArray)).toBeTruthy();
+  });
+  //   it('should fail', () => {
+  //     expect(true).not.toBeTruthy();
+  //   });
+});
